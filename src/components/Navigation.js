d5288c69537428135a1de74c7586f37e7e77b7bd
Julian Ehiem <jules.ehiem@gmail.com>
2021-04-04 07:22:45 -0400

    added navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+const Navigation = () => {
+   return (<div className = "main-nav">
+        <ul>
+            <li>Buttons</li>
+            <li>Cards</li>
+            <li>Footers</li>
+            <li>NavBars</li>
+        </ul>
+    </div>)
+}
+
+export default Navigation;
