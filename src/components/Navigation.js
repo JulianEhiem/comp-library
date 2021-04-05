71211d5d2826e776bea104370c00559df0f719b1
Julian Ehiem <jules.ehiem@gmail.com>
2021-04-05 06:15:30 -0400

    added nav component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
 const Navigation = () => {
-   return (<div className = "main-nav">
+   return (
+   <div className = "main-nav">
+       <div className="title">
+           <h3>Components Library</h3>
+       </div>
         <ul>
-            <li>Buttons</li>
-            <li>Cards</li>
-            <li>Footers</li>
-            <li>NavBars</li>
+            <li><a href="#">Buttons</a></li>
+            <li><a href="#">Cards</a></li>
+            <li><a href="#">Footers</a></li>
+            <li><a href="#">NavBars</a></li>
         </ul>
-    </div>)
+    </div>
+    )
 }
 
 export default Navigation;
